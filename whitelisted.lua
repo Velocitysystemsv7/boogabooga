e7d82299851c34cd88f807b427dc19adc4a902de
Velocitysystemsv7 <138397732+Velocitysystemsv7@users.noreply.github.com>
2023-07-04 00:02:27 +0530

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -1,1 +1,1 @@
-return {"MjY3MTgyMjcxNDk1MzA0NjQ4.KUAJmh.F_MqxSYt6v2ua9ZJpdyncbPALkl"}
+return {"MjY3MTgyMjcxNDk1MzA0NjQ4.KUAJmh.F_MqxSYt6v2ua9ZJpdyncbPALkl","NjE4Mjc4OTczODU0MTg0Mzcw.EOhpAQ.fQVDxyn-_HKOirAs6SXdF3EuYGt"}
